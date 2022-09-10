29fba3581f6fc0767d6bed7b363f6104fc03ba02
richardrsa <richardrafaelsantanna@gmail.com>
2022-09-10 14:47:24 -0300

    add load method to adapter

--- lib/datalayer/usecases/local_storage_adapter.dart
+++ lib/datalayer/usecases/local_storage_adapter.dart
@@ -2,7 +2,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'package:myinventory/datalayer/cache/cache.dart';
 
-class LocalStorageAdapter implements ISaveSecureCacheStorage {
+class LocalStorageAdapter implements ISaveSecureCacheStorage, IFetchSecureCacheStorage {
   final FlutterSecureStorage flutterSecureStorage;
 
   LocalStorageAdapter({required this.flutterSecureStorage});
@@ -10,5 +10,10 @@ class LocalStorageAdapter implements ISaveSecureCacheStorage {
   Future<void> saveSecure(
       {required String? key, required String? value}) async {
     await flutterSecureStorage.write(key: key!, value: value);
+  }
+
+  Future<String> fetchSecure(String key) async {
+    final result = await flutterSecureStorage.read(key: key);
+    return result!;
   }
 }
