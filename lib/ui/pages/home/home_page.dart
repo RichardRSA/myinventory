5514391463c02d6ccb00e6744e421fd3f95c3c05
richardrsa <richardrafaelsantanna@gmail.com>
2022-09-13 12:46:56 -0300

    include pagination on home

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -13,9 +13,12 @@ class HomePage extends StatelessWidget {
 
   HomePage(this.presenter);
 
-  final query = FirebaseFirestore.instance.collection('products').withConverter(
-      fromFirestore: (snapshot, _) => Product.fromJson(snapshot.data()!),
-      toFirestore: (_product, _) => _product.toJson());
+  final query = FirebaseFirestore.instance
+      .collection('products')
+      .orderBy('created', descending: true)
+      .withConverter(
+          fromFirestore: (snapshot, _) => Product.fromJson(snapshot.data()!),
+          toFirestore: (_product, _) => _product.toJson());
 
   @override
   Widget build(BuildContext context) {
@@ -39,58 +42,90 @@ class HomePage extends StatelessWidget {
           backgroundColor: Theme.of(context).primaryColor,
           onPressed: presenter.addNew,
         ),
-        body: Builder(
-          builder: (context) {
-            presenter.isLoadingStream.listen((isLoading) {
-              if (isLoading) {
-                showLoading(context);
-              } else {
-                hideLoading(context);
-              }
-            });
+        body: Builder(builder: (context) {
+          presenter.isLoadingStream.listen((isLoading) {
+            if (isLoading) {
+              showLoading(context);
+            } else {
+              hideLoading(context);
+            }
+          });
 
-            presenter.mainErrorStream.listen((error) {
-              showErrorMessage(context, error);
-            });
+          presenter.mainErrorStream.listen((error) {
+            showErrorMessage(context, error);
+          });
 
-            presenter.navigateToStream.listen((page) {
-              if (page.isNotEmpty == true) {
-                Get.offAllNamed(page);
-              }
-            });
+          presenter.navigateToStream.listen((page) {
+            if (page.isNotEmpty == true) {
+              Get.offAllNamed(page);
+            }
+          });
 
-            return SafeArea(
-              child: FirestoreListView<Product>(
-                  query: query,
-                  // pageSize: 10,
-                  itemBuilder: (context, snapShot) {
-                    final _product = snapShot.data();
-                    return Card(
-                      margin:
-                          EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                      clipBehavior: Clip.antiAlias,
-                      child: Column(
-                        children: [
-                          ListTile(
-                            title: Text(_product.name),
-                            subtitle: Text(_product.code,
-                                style: TextStyle(
-                                    color: Colors.black.withOpacity(0.6))),
-                            trailing: Icon(Icons.delete),
-                          ),
-                          ClipRRect(
-                              borderRadius: BorderRadius.circular(4),
-                              child: CachedNetworkImage(
-                                  imageUrl: _product.image,
-                                  width: 360,
-                                  height: 204,
-                                  fit: BoxFit.fill)),
-                        ],
-                      ),
-                    );
-                  }),
-            );
-          },
-        ));
+          return SafeArea(
+            child: FirestoreQueryBuilder<Product>(
+                query: query,
+                pageSize: 10,
+                builder: (context, snapshot, _) {
+                  if(snapshot.isFetching){
+                    return Center(child: CircularProgressIndicator());
+                  }else if (snapshot.hasError){
+                    return Text('Algo está errado! ${snapshot.error}');
+                  } else {
+                    return GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
+                      itemCount: snapshot.docs.length,
+                      itemBuilder: (context, index) {
+
+                          final hasEndReched = snapshot.hasMore &&
+                          index + 1 == snapshot.docs.length &&
+                          !snapshot.isFetchingMore; 
+
+                          if(hasEndReched){
+                            snapshot.fetchMore();
+                          }
+
+                        final _product = snapshot.docs[index].data();
+                        return ProductWiget(product: _product);
+                      });
+                  }
+                  
+                }),
+          );
+        }));
+  }
+}
+
+class ProductWiget extends StatelessWidget {
+  const ProductWiget({
+    Key? key,
+    required Product product,
+  })  : _product = product,
+        super(key: key);
+
+  final Product _product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(_product.name),
+            subtitle: Text(_product.code,
+                style: TextStyle(color: Colors.black.withOpacity(0.6))),
+            trailing: Icon(Icons.delete),
+          ),
+          ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: CachedNetworkImage(
+                  imageUrl: _product.image,
+                  width: 360,
+                  height: 204,
+                  fit: BoxFit.fill)),
+        ],
+      ),
+    );
   }
 }
